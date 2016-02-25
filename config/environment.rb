322508d53ab1ea14a207910538fcdc9578449724
sawadanorita <sawadano@gmail.com>
2016-02-25 12:27:16 +0000

    finish

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings =
+{
+:user_name => "app47462715@heroku.com",
+:password => "joqgvo9q3260",
+:domain => "heroku.com",
+:address => "smtp.sendgrid.net",
+:port => 587,
+:authentication => :plain,
+:enable_starttls_auto => true
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :blogs
   root "blogs#index"
   get "show/part1" => "show#part1"

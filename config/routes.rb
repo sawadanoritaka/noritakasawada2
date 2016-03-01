2b25a87514930597e14713f9432d64fb0815276c
sawadanorita <sawadano@gmail.com>
2016-03-01 08:02:39 +0000

    bootstrap3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :users
+  resources :users,only: [:index,:show]
   resources :blogs
   root "top#fv"
   get "show/part1" => "show#part1"

d8373708e94e0d80058ccd4856e52b043eb038e2
sawadanorita <sawadano@gmail.com>
2016-03-01 04:46:30 +0000

    bootstrap

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ActiveRecord::Base
- validates :name, :presence => {:message => '名前を入力してください'}
+ validates :name, presence:  {message:  '名前を入力してください'}
  validates :email, :presence => {:message => 'メールアドレスを入力してください'}
 
 end

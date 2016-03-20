b40f72caef11ac035e9edaaca63b519c7a1dcff2
sawadanorita <sawadano@gmail.com>
2016-03-20 14:55:24 +0000

    m

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
 def profile_img(user)
-  if user.provider == 'facebook'
+  if user.image?
+    img_url = user.image
+  elsif user.provider == 'facebook'
     img_url = "https://graph.facebook.com/#{user.uid}/picture?width=100&height=100"
   elsif user.provider == 'twitter'
     img_url = "http://www.paper-glasses.com/api/twipi/#{user.name}/bigger"
   else
-    img_url = user.image
+    
   end
   image_tag(img_url, alt: user.name)
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+    def image_for(user)
+    if user.image?
+       image_tag user.image
+
+      elsif user.provider == "facebook"
+      image_tag "https://graph.facebook.com/#{user.uid}/picture?width=320&height=320"
+
+      elsif user.provider == "twitter"
+      image_tag user.profile_image_url
+
+    else
+      image_tag"sample.jpg"
+    end
+    end
 end
